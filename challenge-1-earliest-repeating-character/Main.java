21234cb80e34d6da571ec15d3ef99b1c1ab4733a
mariam abulail <mariamabulail88@gmail.com>
2026-04-19 17:45:09 +0300

    Improve code readability with comments

--- challenge-1-earliest-repeating-character/Main.java
+++ challenge-1-earliest-repeating-character/Main.java
@@ -2,23 +2,31 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
+        // Input string
         String s = "geeksforgeeks";
+
+        // Call function 
         System.out.println(findFirstRepeating(s));
     }
 
     public static char findFirstRepeating(String s) {
+
+        // HashSet to store characters we have already seen
         HashSet<Character> seen = new HashSet<>();
 
+        // Loop through each character in the string
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
+            // If character already exists in set → it's repeating
             if (seen.contains(ch)) {
-                return ch; 
+                return ch;
             }
 
             seen.add(ch);
         }
 
+        // If no repeating character found, return null character
         return '\0';
     }
 }
