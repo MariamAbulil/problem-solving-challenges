a907e2eb1a7f6e83ab00d9252e9b65e3ec4f2acc
mariam abulail <mariamabulail88@gmail.com>
2026-04-29 14:20:29 +0300

    second approach is sorted and binary search

--- challenge-3-two-sum/Main.java
+++ challenge-3-two-sum/Main.java
@@ -1,6 +1,7 @@
+import java.util.Arrays;
 class Main{
     // solution:#1 Brute force
-    public int[] twoSum(int[] nums,int target){
+    public int[] twoSumUsedBruteForce(int[] nums,int target){
         for(int i = 0 ; i<nums.length;i++){
             for(int j=i+1; j<nums.lengthj++){
                 if (nums[i]+nums[j]==target){
@@ -10,6 +11,30 @@ class Main{
     
         }
         return int[] {}
+    }
+    // 2. Sorting + Binary Search
+     public int[] twoSumUsedSortingBinarySearch(int[] nums, int target) {
+
+        int[] sortedNums = nums.clone();
+        Arrays.sort(sortedNums);
+
+        for (int i = 0; i < sortedNums.length; i++) {
+
+            int complement = target - sortedNums[i];
+
+            int index = Arrays.binarySearch(
+                sortedNums,
+                i + 1,
+                sortedNums.length,
+                complement
+            );
+
+            if (index > i) {
+                return new int[]{i, index};
+            }
+        }
+
+        return new int[]{};
     }
 
 }
